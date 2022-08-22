54c5423330857febd907cb3706829eb2733712db
DeveloperLewis <62842266+DeveloperLewis@users.noreply.github.com>
2022-08-22 05:17:39 +0100

    Setup the view for editing an article, passing in the specified article's details to the form. Ready to post and be edited in the database

--- controllers/article/article_edit.php
+++ controllers/article/article_edit.php
@@ -3,6 +3,15 @@ session_start();
 if(isset($_SESSION['uid'])) {
     if (\classes\models\user\User::isAdmin($_SESSION['uid'])) {
         if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $article_id = $_GET['id'];
+
+            //If the article could not be found via the ID
+            if (!$article = \classes\models\article\Article::getSpecified($article_id)) {
+                //some error to send through to articles panel via sessions
+
+                header('Location: /admin/articles');
+                die();
+            }
             require_once('views/article/edit.php');
         } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "hi";
--- views/article/edit.php
+++ views/article/edit.php
@@ -1,3 +1,7 @@
+<?php
+/* @var $article */
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,10 +31,162 @@
 
                 <div class="col-10">
 
+                    <div id="editarticle-panel">
+                        <form action="/article/edit" method="POST">
+                            <div class="mb-2">
+                                <?php
+                                //Display title errors here if there are any
+                                if (isset($_SESSION['title_errors'])) {
+                                    foreach ($_SESSION['title_errors'] as $k => $v) {
+                                        echo '<div class="alert alert-danger" role="alert">';
+                                        echo $v;
+                                        echo '</div>';
+                                    }
+                                    unset($_SESSION['title_errors']);
+                                }
 
-                    <div id="newarticle-panel">
-                        <p>Here is the article editor panel</p>
+                                ?>
+
+                                <label for="title" class="form-label">Title:</label>
+                                <input type="text" class="form-control" id="title" name="title" value="<?php
+                                if(isset($_SESSION['title_previous'])) { echo $_SESSION['title_previous']; unset($_SESSION['title_previous']); }
+                                else {
+                                    if (isset($article['title'])) {
+                                        echo $article['title'];
+                                    }
+                                }
+                                ?>">
+                            </div>
+
+                            <div class="row mb-2">
+                                <div class="col-2">
+                                    <label for="shared" class="form-label">Shared: </label>
+                                    <select class="form-select" id="shared" name="shared">
+                                        <?php
+                                        if (isset($article['shared'])) {
+                                            if ($article['shared'] == 1) {
+                                                echo '<option selected>Yes</option>';
+                                                echo '<option>No</option>';
+                                            } else {
+                                                echo '<option>Yes</option>';
+                                                echo '<option selected>No</option>';
+                                            }
+                                        }
+                                        ?>">
+                                    </select>
+                                </div>
+
+                                <div class="col-2">
+                                    <label for="categorySelection" class="form-label">Category: </label>
+                                    <select class="form-select" id="categorySelection">
+                                        <?php
+                                        $result = \classes\models\article\Category::getAll();
+
+                                        if (!$result) {
+
+                                        } else {
+                                            foreach ($result as $k => $v) {
+                                                if ($v['category_id'] == $article['category_ids']) {
+                                                    echo '<option value="' . $v['category_id'] . '" selected>' . $v['category_name'] . '</option>';
+                                                }
+
+                                                echo '<option value="' . $v['category_id'] . '">' . $v['category_name'] . '</option>';
+                                            }
+                                        }
+
+                                        ?>
+                                    </select>
+                                </div>
+
+                                <div class="col-2">
+                                    <label for="category" class="form-label">Selected: </label>
+                                    <input class="form-control" type="text" value="" name="category" id="category" readonly>
+                                </div>
+                            </div>
+
+                            <div class="mb-2">
+
+                                <?php
+
+                                //Display body errors here if there are any
+                                if (isset($_SESSION['body_errors'])) {
+                                    foreach ($_SESSION['body_errors'] as $k => $v) {
+                                        echo '<div class="alert alert-danger" role="alert">';
+                                        echo $v;
+                                        echo '</div>';
+                                    }
+                                    unset($_SESSION['body_errors']);
+                                }
+
+                                ?>
+
+                                <label for="body" class="form-label">Body: </label>
+                                <textarea class="form-control" id="body" name="body" style="resize: none; height: 600px;"><?php
+                                    if(isset($_SESSION['body_previous'])) { echo $_SESSION['body_previous']; unset($_SESSION['body_previous']); }
+                                    else {
+                                        if (isset($article['body'])) {
+                                            echo $article['body'];
+                                        }
+                                    }
+                                    ?></textarea>
+                            </div>
+
+                            <div class="mb-2">
+
+                                <?php
+
+                                //Display notes errors here if there are any
+                                if (isset($_SESSION['notes_errors'])) {
+                                    foreach ($_SESSION['notes_errors'] as $k => $v) {
+                                        echo '<div class="alert alert-danger" role="alert">';
+                                        echo $v;
+                                        echo '</div>';
+                                    }
+                                    unset($_SESSION['notes_errors']);
+                                }
+
+                                ?>
+
+                                <label for="notes" class="form-label">Notes: </label>
+                                <textarea class="form-control" id="notes" name="notes" style="resize: none; height: 200px;"><?php
+                                    if(isset($_SESSION['notes_previous'])) { echo $_SESSION['notes_previous']; unset($_SESSION['notes_previous']); }
+                                    else {
+                                        if (isset($article['notes'])) {
+                                            echo $article['notes'];
+                                        }
+                                    }
+                                    ?></textarea>
+                            </div>
+
+                            <div>
+                                <button class="btn btn-primary" type="submit">Edit article</button>
+                                <button class="btn btn-danger" type="button" id="cancel-button">Cancel</button>
+                            </div>
+                        </form>
+
+                        <script>
+                            let cancelButton = document.getElementById('cancel-button');
+                            let categorySelection = document.getElementById('categorySelection');
+
+                            document.getElementById('category').value = categorySelection.value
+
+                            cancelButton.addEventListener('click', function() {
+                                let c = confirm('Are you sure you want to cancel editing this article?');
+
+                                if (c) {
+                                    console.log('User clicked ok')
+                                    window.location.href = "/admin/dashboard"
+                                } else {
+                                    console.log('user clicked cancel')
+                                }
+                            }, false);
+
+                            categorySelection.addEventListener('change', function() {
+                                document.getElementById('category').value = categorySelection.value
+                            })
+                        </script>
                     </div>
+
 
                 </div>
             </div>
