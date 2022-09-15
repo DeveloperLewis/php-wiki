a9cf0865a8bf37fe193eefe668725d7f7c6990d5
DeveloperLewis <62842266+DeveloperLewis@users.noreply.github.com>
2022-09-15 03:39:49 +0100

    Added a new validation rule

--- classes/Validation.php
+++ classes/Validation.php
@@ -50,7 +50,7 @@ class Validation
             $body_errors['max_size'] = "The body must be less than 5000000 characters. You're currently at: " . strlen($body) . ".";
         }
 
-        if (preg_match('/[^a-zA-Z\d#\-=.,:;\/*@!\[\]()_`<>\s%"]/', $body)) {
+        if (preg_match('/[^a-zA-Z\d#\-=.,:;\/*@!\[\]()_`\'<>\s%"]/', $body)) {
             $body_errors['special_chars'] = "The body can only contain letters, numbers and these
             special characters: #-=.,:;/_*@![]()`<>% as well as quotation marks.";
         }
